d68e0dd48b836c4cdadd52e64730a175d10bfd01
yonetaku0519 <yonezawa051319@gmail.com>
2021-11-30 11:36:45 +0900

    Java biggner 02

---
+++ Bginner_02/src/calc.java
@@ -0,0 +1,9 @@
+
+public class calc {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 20;
+		System.out.println("答えは" + (x + y));
+	}
+
+}
---
+++ Bginner_02/src/work02.java
@@ -0,0 +1,8 @@
+
+public class work02 {
+	public static void main(String[] args) {
+		
+		System.out.println(1 + 1);
+	}
+
+}
