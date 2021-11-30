2e1918c26ae57ec557dd084cc81dd3a0ae48defa
yonetaku0519 <yonezawa051319@gmail.com>
2021-11-30 10:44:15 +0900

    test

---
+++ Hello/src/Work1.java
@@ -0,0 +1,6 @@
+
+public class Work1 {
+	public static void main(String[] args) {
+		System.out.println("私の名前は〇〇です！！");
+	}
+}
---
+++ Hello/src/Work2.java
@@ -0,0 +1,8 @@
+
+public class Work2 {
+	public static void main(String[] args) {
+		System.out.println("私の名前は〇〇です");
+		System.out.println("今Javaの学習をしています");
+	}
+
+}
