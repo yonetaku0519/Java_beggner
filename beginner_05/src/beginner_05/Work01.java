5dd5890353bd79c4e9d7da7fc68b833a0b208b97
yonetaku0519 <yonezawa051319@gmail.com>
2021-12-03 10:28:16 +0900

    beginner05

---
+++ beginner_05/src/beginner_05/Work01.java
@@ -0,0 +1,20 @@
+package beginner_05;
+
+public class Work01 {
+
+	public static void main(String[] args) {
+		for(int i = 0; i < 5; i++) {
+			System.out.println("Hello!! 1");
+		}
+		
+		for(int j = 0; j < 10; j++) {
+			System.out.println("Hello!! 2");
+		}
+		
+		for(int k = 1; k <= 10; k++) {
+			System.out.println("Hello!! 3");
+		}
+
+	}
+
+}
