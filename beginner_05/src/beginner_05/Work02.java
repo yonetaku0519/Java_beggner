54f6c5eae0d5bede3834c407f6c32c31e852f69a
yonetaku0519 <yonezawa051319@gmail.com>
2021-12-03 10:46:25 +0900

    Work02

---
+++ beginner_05/src/beginner_05/Work02.java
@@ -0,0 +1,24 @@
+package beginner_05;
+
+public class Work02 {
+	
+	public static void main(String[] args) {
+		int i = 1;
+		
+		while(i < 5) {
+			System.out.println(i);
+			i++;
+		}
+		
+		
+		int j = 1;
+		
+		while(j < 50) {
+			System.out.println(j);
+			j = j * 2;
+		}
+		
+		
+	}
+
+}
